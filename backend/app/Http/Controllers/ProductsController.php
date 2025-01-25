7d8e09afa0eeed2df9f9fe9a9afeb741160b8cc4
Juan Daniel Gonzalez <112042600+JuanDanielGonzalez4@users.noreply.github.com>
2025-01-25 10:17:39 -0500

    feat: Add complete basic CRUD functionality

--- backend/app/Http/Controllers/ProductsController.php
+++ backend/app/Http/Controllers/ProductsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Http\Requests\ProductRequest;
+
 
 class ProductsController extends Controller
 {
@@ -17,19 +19,19 @@ class ProductsController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        
+        $product = new Product();
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->quantity = $request->quantity;
+        $product->save();
+
+        return response()->json(['message' => 'Product created'], 201);
     }
 
     /**
@@ -37,23 +39,34 @@ class ProductsController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        return response()->json($product, 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
     {
-        //
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        $product->name = $request->name ?? $product->name;
+        $product->description = $request->description ?? $product->description;
+        $product->price = $request->price ?? $product->price;
+        $product->quantity = $request->quantity ?? $product->quantity;
+        $product->save();
+
+        return response()->json(['message' => 'Product updated', 'product' => $product], 200);
     }
 
     /**
@@ -61,6 +74,14 @@ class ProductsController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $product = Product::find($id);
+
+        if(!$product) {
+            return response()->json(['message' => 'Product not found'], 404);
+        }
+
+        $product->delete();
+
+        return response()->json(['message' => 'Product deleted successfully'], 200);
     }
 }
